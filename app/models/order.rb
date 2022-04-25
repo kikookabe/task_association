5081f9070aee6a69eb3b63c808f22e97271a44de
kiko <kiko_okabe@mediado.jp>
2022-04-25 18:02:28 +0900

    third commit

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ApplicationRecord
     has_many :order_foods
     belongs_to :address
     belongs_to :customer
+    belongs_to :foods
 end
--- db/migrate/20171108085125_create_orders.rb
+++ db/migrate/20171108085125_create_orders.rb
@@ -3,6 +3,7 @@ class CreateOrders < ActiveRecord::Migration[5.1]
     create_table :orders do |t|
       t.references :user, null: false, foreign_key: true
       t.references :address, null: false, foreign_key: true
+      t.references :food, null: false, foreign_key: true
 
       t.timestamps
     end
