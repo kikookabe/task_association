de3d4f567b0a502e66f214f5c283f1784c40f5cd
kiko <kiko_okabe@mediado.jp>
2022-04-25 15:34:58 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210126090143) do
+ActiveRecord::Schema.define(version: 2022_04_25_060550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20210126090143) do
     t.index ["shop_id"], name: "index_foods_on_shop_id"
   end
 
+  create_table "foods_orders", force: :cascade do |t|
+    t.bigint "food_id", null: false
+    t.bigint "order_id", null: false
+    t.index ["food_id"], name: "index_foods_orders_on_food_id"
+    t.index ["order_id"], name: "index_foods_orders_on_order_id"
+  end
+
   create_table "order_foods", force: :cascade do |t|
     t.bigint "food_id"
     t.bigint "order_id"
@@ -70,6 +77,8 @@ ActiveRecord::Schema.define(version: 20210126090143) do
   end
 
   add_foreign_key "foods", "shops"
+  add_foreign_key "foods_orders", "foods"
+  add_foreign_key "foods_orders", "orders"
   add_foreign_key "order_foods", "foods"
   add_foreign_key "order_foods", "orders"
   add_foreign_key "orders", "customers"
